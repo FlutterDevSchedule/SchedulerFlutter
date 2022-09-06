66880250cb6b575aaae3e339ab57784e8a7c09b2
BTN3576\48781 <rafal.sztorc@britenet.com.pl>
2022-09-06 15:31:32 +0200

    Add event popup + style

--- lib/sheduler_main_layout.dart
+++ lib/sheduler_main_layout.dart
@@ -43,8 +43,8 @@ class _MainLayout extends State<MainLayout> {
                                 child: Container(
                                   color: Color(0xFF20B878),
                                   child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment
-                                          .start,
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
                                       children: <Widget>[
                                         const Expanded(
                                           //child: Container(
@@ -62,7 +62,9 @@ class _MainLayout extends State<MainLayout> {
                                                 CrossAxisAlignment.start,
                                                 children: const <Widget>[
                                                   Padding(
-                                                    padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                                                    padding: EdgeInsets
+                                                        .fromLTRB(
+                                                        30, 0, 0, 0),
                                                     child: Expanded(
                                                         child: Text(
                                                             'Sala konferencyjna',
@@ -72,18 +74,22 @@ class _MainLayout extends State<MainLayout> {
                                                                 fontSize: 25,
                                                                 fontWeight:
                                                                 FontWeight
-                                                                    .w600))),
+                                                                    .w600))
+                                                    ),
                                                   ),
                                                   Padding(
-                                                    padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                                                    padding: EdgeInsets
+                                                        .fromLTRB(
+                                                        30, 0, 0, 0),
                                                     child: Expanded(
                                                       child: Text(
                                                         'Analize room',
                                                         style: TextStyle(
                                                           color: Colors.white,
                                                           fontSize: 70,
-                                                          fontWeight: FontWeight
-                                                              .w800,),
+                                                          fontWeight:
+                                                          FontWeight.w800,
+                                                        ),
                                                       ),
                                                     ),
                                                   )
@@ -94,19 +100,42 @@ class _MainLayout extends State<MainLayout> {
                                 flex: 2,
                                 child: Container(
                                     color: Color(0xFF003F2B),
-                                    child: Center(
+                                    child: const Center(
                                       child: Text('Sala wolna do końca dnia',
                                           style: TextStyle(
                                               color: Colors.white,
                                               fontSize: 30,
-                                              fontWeight: FontWeight.bold
-                                          )),
-                                    )
-                                ))
+                                              fontWeight: FontWeight.bold)),
+                                    )))
                           ])),
                   Expanded(flex: 3, child: Container(child: AppoitmentBar())),
                 ])),
           ],
-        ));
+        ),
+        floatingActionButton: FloatingActionButton.large(
+            onPressed: () =>
+                showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        AlertDialog(
+                          title: const Text('Create new event'),
+                          content: Container(
+                              width: 900,
+                              child: LoggedInPage(user: widget.user,)),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, 'Cancel'),
+                              child: const Text('Back'),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, 'OK'),
+                              child: const Text('Save event'),
+                            ),
+                          ],
+                        ),
+                ),
+
+            child: Text('Add event'),
+        backgroundColor: Colors.blue));
   }
 }
